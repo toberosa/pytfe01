aa5abf97c3180d4504fd868586f9e1d5757f3bf1
toberosa <55308481+toberosa@users.noreply.github.com>
2023-09-12 00:38:54 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+terraform {
+
+#  backend "s3" {
+#    bucket = "statefile11092023"
+#    key    = "aws/statefiles/demo.tfstate"
+#    region = "ap-southeast-1"
+#  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+resource "aws_instance" "web0Z" {
+  ami           = "ami-0464f90f5928bccb8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "web0Z"
+  }
+
+  tags_all = {
+    Name = "web0Z"
+  }
+
+}
