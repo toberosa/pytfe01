5bb09c713a5fc1e9f95afdd30b63c869485fc572
toberosa <55308481+toberosa@users.noreply.github.com>
2023-09-12 00:42:47 +0800

    Update main.tf

--- pytfe/main.tf
+++ pytfe/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 resource "aws_instance" "web0Z" {
   ami           = "ami-0464f90f5928bccb8"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "web0Z"
